3e7afec164556d1b4e7c57534f18a9b44a724f3d
AlanaSann <109287005+AlanaSann@users.noreply.github.com>
2023-08-15 16:16:27 -0300

    Update Q9.py

--- Q9.py
+++ Q9.py
@@ -1,3 +1,10 @@
+import math
 base = 10
 altura = 5
-area = base*altura
+perimetro = (2*base) + (2*altura)
+area = base * altura
+diagonal = math.sqrt((10**2) + (5**2))
+
+print("Perimetro: ",perimetro)
+print("Area: ",area)
+print("Diagonal: ",diagonal)
